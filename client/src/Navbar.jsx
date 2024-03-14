58034dfa0bd276755ebdc8a133366ecd35f8374f
sayanbose-0000 <bosesayan0000@gmail.com>
2024-03-14 07:25:45 +0530

    added stuffs

--- client/src/Navbar.jsx
+++ client/src/Navbar.jsx
@@ -1,41 +1,46 @@
-  import { useEffect, useState } from 'react';
-  import '../styles/navbar.css';
-  import { Link } from "react-router-dom";
+import { useEffect, useContext } from 'react';
+import '../styles/navbar.css';
+import { Link } from "react-router-dom";
+import { userContext } from './main';
 
-  const Navbar = () => {
-    const [userInfo, setUserInfo] = useState('');
+const Navbar = () => {
+  const { setUserInfo, userInfo } = useContext(userContext);
 
-    useEffect(() => {
-      fetch('http://localhost:3000/profile', {
-        method: "GET",
-        headers: { "Content-Type": "application/json" },
-        credentials: 'include', // since we want to use cookie, we need to use this
-      }).then((res) => {
-        res.json().then((info) => {
-          setUserInfo(info);
-        })
+  const handleLogOut = (e) => {
+    e.preventDefault();
+  }
+
+  useEffect(() => {
+    fetch('http://localhost:3000/profile', {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+      credentials: 'include', // since we want to use cookie, we need to use this
+    }).then((res) => {
+      res.json().then((info) => {
+        setUserInfo(info);
       })
-      console.log(userInfo)
-    }, [])
+    })
+  }, [])
 
-    return (
-      <div className="navbar">
-        <Link to="/" className='goodnotes'>Snote</Link>
-        {/*<a href="" className='goodnotes'>goodnotes</a>*/}
-        {
-          userInfo!=="Invalid token" ?
-            <div className="register">
-              <p className='username'>{userInfo.username}</p>
-              <button className='logout'>LogOut</button>
-            </div>
-            :
-            <div className="register">
-              <Link className='login' to='/login'>Login</Link>
-              <Link className='signup' to='/signup'>Signup</Link>
-            </div>
-        }
-      </div>
-    );
-  };
+  const userName = userInfo?.username;
 
-  export default Navbar;
+  return (
+    <div className="navbar">
+      <Link to="/" className='goodnotes'>Snote</Link>
+      {
+        userName ?
+          <div className="register">
+            <p className='username'>{userInfo.username}</p>
+            <button className='logout' onClick={(e) => handleLogOut(e)} >LogOut</button>
+          </div>
+          :
+          <div className="register">
+            <Link className='login' to='/login'>Login</Link>
+            <Link className='signup' to='/signup'>Signup</Link>
+          </div>
+      }
+    </div>
+  );
+};
+
+export default Navbar;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,34 +1,48 @@
-import React from 'react'
+import React, { createContext, useState } from 'react'
 import ReactDOM from 'react-dom/client'
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ErrorPage from './ErrorPage.jsx';
 import Home from "./Home.jsx";
 import Layout from "./Layout.jsx";
 import Login from "./Login.jsx";
 import Signup from "./Signup.jsx";
 import NewNote from "./NewNote.jsx";
+import { useContext } from 'react';
+
+
+// used for useContext
+export const userContext = createContext({});
+
+const UserContextProvider = ({children}) => {
+  const [userInfo, setUserInfo] = useState({});
+  return (
+    <userContext.Provider value={{ userInfo, setUserInfo }}>
+      {children}
+    </userContext.Provider>
+  )
+}
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Layout/>,
-    errorElement: <ErrorPage/>,
+    element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: "/",
-        element: <Home/>
+        element: <Home />
       },
       {
         path: "/login",
-        element: <Login/>
+        element: <Login />
       },
       {
         path: "/signup",
-        element: <Signup/>
+        element: <Signup />
       },
       {
         path: "/addnote",
-        element: <NewNote/>
+        element: <NewNote />
       },
 
     ]
@@ -38,6 +52,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <UserContextProvider>
+      <RouterProvider router={router} />
+    </UserContextProvider>
   </React.StrictMode>,
 )
