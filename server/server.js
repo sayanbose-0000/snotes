5a100d79ed28310b5291ddae7753096f635428dc
sayanbose-0000 <bosesayan0000@gmail.com>
2024-03-31 22:53:30 +0530

    added

--- server/server.js
+++ server/server.js
@@ -77,10 +77,10 @@ app.post('/server/login', async (req, res) => {
       // console.log(token);
       res.cookie('token', token, {
         path: '/', // ensures cookie is available in entire app
-        httpOnly: true,
-        maxAge: 30 * 60 * 60 * 24 * 1000,
-        sameSite: process.env.NODE_ENV === "Development" ? "lax" : "none",
-        secure: process.env.NODE_ENV === "Development" ? false : true
+        // httpOnly: true,
+        // maxAge: 30 * 60 * 60 * 24 * 1000,
+        // sameSite: process.env.NODE_ENV === "Development" ? "lax" : "none",
+        // secure: process.env.NODE_ENV === "Development" ? false : true
       });
       res.status(200).json("Successfully signed up");
     }
@@ -108,8 +108,10 @@ app.get('/server/profile', async (req, res) => {
 
 // ------- logout -------
 app.post('/server/logout', (req, res) => {
-  // res.cookie('token', '').json(ok);
-  res.clearCookie('token', { path: "/" }).json(ok);
+  // res.cookie('token', '').json(ok);    
+  res.clearCookie('token', {
+    path: "/"
+  }).json(ok);
 })
 
 
